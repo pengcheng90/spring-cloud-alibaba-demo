da54cd60ab28b4dc100fd535c614f0a2381b63f0
xie <1298568873@qq.com>
2022-08-30 19:20:23 +0800

    Initial commit

---
+++ provider/src/main/java/com/peng/springcloudalibaba/demo/provider/controller/EchoController.java
@@ -0,0 +1,15 @@
+package com.peng.springcloudalibaba.demo.provider.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EchoController {
+
+    @GetMapping("/echo/{string}")
+    public String echo(@PathVariable String string) {
+        return "Hello Nacos Discover "+ string;
+    }
+
+}
